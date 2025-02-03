01af2b8ad24f3d7bb95ffca434d2b7ae265378cb
AYO ELUTILO <ayoelutilo@gmail.com>
2025-02-03 03:52:49 -0500

    chore: maintenance and minor fixes

--- internal/httpapi/server.go
+++ internal/httpapi/server.go
@@ -222,3 +222,5 @@ func writeJSON(w http.ResponseWriter, statusCode int, payload any) {
 func writeError(w http.ResponseWriter, statusCode int, message string) {
 	writeJSON(w, statusCode, map[string]any{"error": message})
 }
+
+// Refinement.
